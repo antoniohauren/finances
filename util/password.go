535b873673e5e42e9723705909766c3627ef3da0
antoniohauren <antoniohauren@protonmail.com>
2025-09-15 13:21:12 -0300

    feat: add password utils

---
+++ util/password.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(pw string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(pw), bcrypt.DefaultCost)
+
+	if err != nil {
+		return "", fmt.Errorf("error hashing the password %w", err)
+	}
+
+	return string(hashed), nil
+}
+
+func CheckPassword(pw string, hashed string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashed), []byte(pw))
+}
