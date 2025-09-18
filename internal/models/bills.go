ac9c59a5b008160d4cd820030a7c392b34c6bbb3
antoniohauren <antoniohauren@protonmail.com>
2025-09-18 11:00:48 -0300

    chore: move to enums to make it more clean

--- internal/models/bills.go
+++ internal/models/bills.go
@@ -6,42 +6,6 @@ import (
 	"github.com/google/uuid"
 )
 
-type BillType string
-
-const (
-	BillTypeFixedCosts       BillType = "FIXED_COSTS"       // Custos fixos (30%)
-	BillTypeComfort          BillType = "COMFORT"           // Conforto (15%)
-	BillTypeGoals            BillType = "GOALS"             // Metas (15%)
-	BillTypePleasures        BillType = "PLEASURES"         // Prazeres (10%)
-	BillTypeFinancialFreedom BillType = "FINANCIAL_FREEDOM" // Liberdade financeira (25%)
-	BillTypeKnowledge        BillType = "KNOWLEDGE"         // Conhecimento (5%)
-)
-
-type BillCategory string
-
-const (
-	BillCategoryFood          BillCategory = "FOOD"
-	BillCategoryUtilities     BillCategory = "UTILITIES"
-	BillCategoryHousing       BillCategory = "HOUSING"
-	BillCategoryTransport     BillCategory = "TRANSPORT"
-	BillCategoryEntertainment BillCategory = "ENTERTAINMENT"
-	BillCategoryHeathcare     BillCategory = "HEALTHCARE"
-	BillCategorySavings       BillCategory = "SAVINGS"
-	BillCategoryTaxes         BillCategory = "TAXES"
-	BillCategoryOthers        BillCategory = "OTHERS"
-)
-
-type BillFrequency string
-
-const (
-	BillFrequencyYearly   BillFrequency = "YEARLY"
-	BillFrequencyMontly   BillFrequency = "MONTLY"
-	BillFrequencyWeekly   BillFrequency = "WEEKLY"
-	BillFrequencyBiweekly BillFrequency = "BIWEEKLY"
-	BillFrequencyDaily    BillFrequency = "DAILY"
-	BillFrequencyOther    BillFrequency = "OTHER"
-)
-
 type Bill struct {
 	BaseEntity
 	Name          string
--- internal/models/enums.go
+++ internal/models/enums.go
@@ -9,3 +9,101 @@ const (
 	BillPaymentMethodPix        BillPaymentMethod = "PIX"
 	BillPaymentMethodOther      BillPaymentMethod = "OTHER"
 )
+
+func (e BillPaymentMethod) IsValid() bool {
+	switch e {
+	case
+		BillPaymentMethodCreditCard,
+		BillPaymentMethodDebitCard,
+		BillPaymentMethodCash,
+		BillPaymentMethodPix,
+		BillPaymentMethodOther:
+		return true
+	default:
+		return false
+	}
+}
+
+type BillType string
+
+const (
+	BillTypeFixedCosts       BillType = "FIXED_COSTS"       // Custos fixos (30%)
+	BillTypeComfort          BillType = "COMFORT"           // Conforto (15%)
+	BillTypeGoals            BillType = "GOALS"             // Metas (15%)
+	BillTypePleasures        BillType = "PLEASURES"         // Prazeres (10%)
+	BillTypeFinancialFreedom BillType = "FINANCIAL_FREEDOM" // Liberdade financeira (25%)
+	BillTypeKnowledge        BillType = "KNOWLEDGE"         // Conhecimento (5%)
+)
+
+func (e BillType) IsValid() bool {
+	switch e {
+	case
+		BillTypeFixedCosts,
+		BillTypeComfort,
+		BillTypeGoals,
+		BillTypePleasures,
+		BillTypeFinancialFreedom,
+		BillTypeKnowledge:
+		return true
+	default:
+		return false
+	}
+}
+
+type BillCategory string
+
+const (
+	BillCategoryFood          BillCategory = "FOOD"
+	BillCategoryUtilities     BillCategory = "UTILITIES"
+	BillCategoryHousing       BillCategory = "HOUSING"
+	BillCategoryTransport     BillCategory = "TRANSPORT"
+	BillCategoryEntertainment BillCategory = "ENTERTAINMENT"
+	BillCategoryHeathcare     BillCategory = "HEALTHCARE"
+	BillCategorySavings       BillCategory = "SAVINGS"
+	BillCategoryTaxes         BillCategory = "TAXES"
+	BillCategoryOthers        BillCategory = "OTHERS"
+)
+
+func (e BillCategory) IsValid() bool {
+	switch e {
+	case
+		BillCategoryFood,
+		BillCategoryUtilities,
+		BillCategoryHousing,
+		BillCategoryTransport,
+		BillCategoryEntertainment,
+		BillCategoryHeathcare,
+		BillCategorySavings,
+		BillCategoryTaxes,
+		BillCategoryOthers:
+		return true
+	default:
+		return false
+	}
+}
+
+type BillFrequency string
+
+const (
+	BillFrequencyYearly   BillFrequency = "YEARLY"
+	BillFrequencyMontly   BillFrequency = "MONTLY"
+	BillFrequencyWeekly   BillFrequency = "WEEKLY"
+	BillFrequencyBiweekly BillFrequency = "BIWEEKLY"
+	BillFrequencyDaily    BillFrequency = "DAILY"
+	BillFrequencyOther    BillFrequency = "OTHER"
+)
+
+func (e BillFrequency) IsValid() bool {
+	switch e {
+	case
+		BillFrequencyYearly,
+		BillFrequencyMontly,
+		BillFrequencyWeekly,
+		BillFrequencyBiweekly,
+		BillFrequencyDaily,
+		BillFrequencyOther:
+		return true
+	default:
+		return false
+	}
+}
